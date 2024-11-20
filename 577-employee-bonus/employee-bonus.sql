463a03d9003fb2a78c7d57a912e115288bbcd821
Himanshu Kudesiya <124152041+ashu-kudesiya@users.noreply.github.com>
2024-11-20 17:31:40 +0530

    Time: 1124 ms (44.04%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,5 +1,10 @@
-SELECT name, bonus
-FROM Employee
-LEFT JOIN Bonus
-ON Employee.empID = Bonus.empID
-WHERE bonus < 1000 OR Bonus IS NULL ;
+SELECT 
+    name, bonus
+FROM 
+    Employee
+LEFT JOIN 
+    Bonus
+ON 
+    Employee.empID = Bonus.empID
+WHERE 
+    bonus < 1000 OR Bonus IS NULL ;
