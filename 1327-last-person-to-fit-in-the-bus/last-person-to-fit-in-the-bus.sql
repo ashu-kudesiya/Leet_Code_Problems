c7fb5b5e773577d35567a46cee9681f407c5d5d7
Himanshu Kudesiya <124152041+ashu-kudesiya@users.noreply.github.com>
2024-12-12 18:32:33 +0530

    Time: 1634 ms (31.34%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,15 +1,3 @@
-SELECT
-    Q1.person_name
-FROM
-    Queue Q1
-JOIN
-    Queue Q2
-ON
-    Q1.turn >= Q2.turn
-GROUP BY
-    Q1.turn
-HAVING
-    SUM(Q2.weight) <= 1000
-ORDER BY
-    SUM(Q2.weight) DESC
-LIMIT 1
+SELECT person_name from (SELECT person_name,turn,
+sum(weight) over (order by turn) AS cum FROM queue) p1
+where cum<=1000 order by turn DESC limit 1
