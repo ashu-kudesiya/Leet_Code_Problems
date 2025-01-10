262d4d24d6d305431d0d2120b42cea0f62dfe20d
Himanshu Kudesiya <124152041+ashu-kudesiya@users.noreply.github.com>
2025-01-10 20:01:22 +0530

    Time: 1416 ms (18.47%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -2,4 +2,4 @@ SELECT p.product_name AS product_name, sum(o.unit) AS unit FROM Products p
 JOIN Orders o USING (product_id)
 WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='02'
 GROUP BY p.product_id
-HAVING SUM(o.unit)>=100
+HAVING SUM(o.unit)>=100;
